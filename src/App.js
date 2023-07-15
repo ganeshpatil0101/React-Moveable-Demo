6e54e2cda6cb7b07fc0ae64f49f8a609cc69185f
ganeshpatil0101 <ganeshpatil0101@gmail.com>
2023-07-15 20:07:14 +0530

    scale & wrap implementation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React from "react";
+import React from 'react';
 import Demo from './demo';
-import "./style.css";
+import './style.css';
 
 export default function App() {
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
+      <h1>Calculator</h1>
       <Demo />
     </div>
   );
--- src/demo.jsx
+++ src/demo.jsx
@@ -3,19 +3,48 @@ import Moveable from 'react-moveable';
 
 export default function Demo() {
   const targetRef = React.useRef();
-
+  const [boxText, setBoxText] = React.useState('w:200 X h:200');
   return (
     <div className="root">
       <div className="container">
         <div className="target" ref={targetRef}>
-          Target
+          <div className="box">
+            <span>{boxText}</span>
+          </div>
         </div>
         <Moveable
           target={targetRef}
+          scalable={false}
           warpable={true}
+          draggable={true}
+          throttleDrag={1}
+          edgeDraggable={false}
+          startDragRotate={0}
+          throttleDragRotate={0}
+          onDrag={(e) => {
+            e.target.style.transform = e.transform;
+          }}
+          throttleScale={0}
+          onScale={(e) => {
+            e.target.style.transform = e.drag.transform;
+            const cValues = e.target.getBoundingClientRect();
+            // console.log(cValues.height, cValues.width);
+            setBoxText(
+              `w:${Math.round(cValues.width)} X h:${Math.round(
+                cValues.height
+              )} `
+            );
+          }}
           renderDirections={['nw', 'n', 'ne', 'w', 'e', 'sw', 's', 'se']}
           onWarp={(e) => {
             e.target.style.transform = e.transform;
+            const cValues = e.target.getBoundingClientRect();
+            console.log(cValues.height, cValues.width);
+            setBoxText(
+              `w:${Math.round(cValues.width)} X h:${Math.round(
+                cValues.height
+              )} `
+            );
           }}
         />
       </div>
